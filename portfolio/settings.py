774c43165607b8fd3453356ac83d338a3747478b
Diki Muhamad SYidik <dikimuhamadsyidik14@hotmail.com>
2018-03-22 14:50:18 +0700

    'jikoaaa'

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'app/staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'app/app/staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
