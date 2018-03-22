af3e95bb1906e10ac23d0912b7415e16319dbddd
Diki Muhamad SYidik <dikimuhamadsyidik14@hotmail.com>
2018-03-22 15:15:40 +0700

    'ashdbawkd'

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -20,4 +20,9 @@ from portfolio_app import views
 urlpatterns = [
     url(r'^$', views.index,name='index'),
     url(r'^admin/', admin.site.urls),
+    
 ]
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT, 'show_indexes': True }),
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, 'show_indexes': True }),)
